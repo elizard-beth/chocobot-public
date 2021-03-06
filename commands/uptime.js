2db8d4e604e19aea85526191910032e6969ef270
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-03-06 13:32:33 +1300

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,5 +1,14 @@
 module.exports = {
     execute(msg, _, __, ___, ____, client) {
-        msg.channel.send(Math.floor(client.uptime / 60000) + " minutes.");
+        if (client.uptime / 60000 < 60) {
+            msg.channel.send(Math.floor(client.uptime / 60000) + " minutes.");
+        }
+        else if (client.uptime / 600000 < 60) {
+            msg.channel.send(Math.floor(client.uptime / 600000) + " hours.");
+        }
+        else if (client.uptime / 600000 / 24 > 1) {
+            msg.channel.send(Math.floor(client.uptime / 600000 / 24) + " days.");
+        }
+
     }
 }
