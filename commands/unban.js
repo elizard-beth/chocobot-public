d23a65faa83209f8fe476082626724f6f64ce85e
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-03-09 19:27:22 +1300

    Create unban.js

---
+++ commands/unban.js
@@ -0,0 +1,23 @@
+  
+module.exports = {
+  execute(msg) {
+    try{
+      msg.guild.fetchBans().then(banned=>{
+         embed
+            .setThumbnail()
+            .setColor('#0099ff')
+            .setTitle('USER | UBBANNED')
+            .setDescription("`Unbanned: \nid: " + msg.content.split(" ")[1] + "`\n");
+            fs.readFile('users.json', 'utf8', function(err, data){ 
+               try { client.channels.cache.get(JSON.parse(data)[msg.guild.id]["logs"]).send(embed); } catch { }
+            })  
+        msg.guild.members.unban(banned.find(Number(msg.content.split(" ")[1])))
+        msg.channel.send("This person is unbanned :blush:!")
+      })
+    }
+    catch(e){
+      msg.channel.send("I could not unban this person.");
+      console.log(e);
+    }
+  }
+}
