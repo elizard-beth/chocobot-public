e5c8cf7a6070101906cdf8283624c5ada51eafc1
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-03-12 00:02:10 +1300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,7 +1,8 @@
+  
 const fs = require('fs')
 module.exports = {
-    execute(msg, _, __, ___, ____, _______) {
-        if (msg.member.hasPermission("KICK_MEMBERS")) {
+    execute(msg, _, __, ___, ____, _______) {try{
+        if (msg.member.hasPermission("BAN_MEMBERS")) {
             try {
                 msg.mentions.members.first().kick();
                 embed
@@ -12,10 +13,7 @@ module.exports = {
                 fs.readFile('users.json', 'utf8', function(err, data){ 
                  try { client.channels.cache.get(JSON.parse(data)[msg.guild.id]["logs"]).send(embed); } catch { }
                 })  
-              } 
-              catch {
-                msg.channel.send("I ran into some trouble trying to perform this operation.");
-              }
-        }
+              } catch {}
+        }}catch{}
     }
 }
