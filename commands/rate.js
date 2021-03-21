72d1d75a14c5dc1b47bdb1d64035890631efacc9
widesteal321 <71954340+widesteal321@users.noreply.github.com>
2021-03-21 17:21:26 +1300

    Update rate.js

--- commands/rate.js
+++ commands/rate.js
@@ -1,5 +1,5 @@
 module.exports = {
-    execute(msg, _, __, ___, arrays, _____) {
+    execute(msg, _, __, ___, arrays) {
       let rating = arrays["rating"]
       msg.channel.send(rating[Math.floor(Math.random() * 10)]);
     }
